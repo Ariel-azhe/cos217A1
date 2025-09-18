2fce00db88a747cac7e65f549dadbca5827c4ad8
Ariel Yuan <ay3052@princeton.edu>
2025-09-18 17:46:02 -0400

    added readme and dfa files

--- decomment.c
+++ decomment.c
@@ -15,7 +15,6 @@ int escape_squoted(void);
 
 int error_line = 1;
 int comment_start = 0;
-/*int comment_line = 0;*/
 
 /*takes no argument; automatically called at start of program
 calls the start_state function to begin the DFA calls*/
@@ -26,17 +25,16 @@ int main(void)
 }
 
 /*gets the next character from input.
-If at end of file, end program; otherwise, 
-enter corresponding states if next char
-is /, ", or ', and print all other chars as they are  */
+Terminates program if reached end of input;
+otherwise, enter corresponding states if next char
+is a special character, 
+and print all other chars as they are  */
 int start_state(void)
 {
     int ch;
-    /*printf("start_state() ");*/
     ch = getchar();
     if (ch == '\n')
     {
-        /*printf("new line");*/
         error_line=error_line+1;
     }
     if (ch != EOF)
@@ -44,7 +42,6 @@ int start_state(void)
         switch (ch)
         {
             case '/':
-                /*printf("left slash");*/
                 state_left_slash();
                 break;
             case '\"':
@@ -64,15 +61,14 @@ int start_state(void)
     return 0;
 }
 /*Gets the next character from input.
-Previous char is /. 
-Enter corresponding states if next char is 
-*, /, ", or ', otherwise, print next char as it is
+Enters a comment if next char is *,
+enter corresponding states if next char is 
+a special character, otherwise, print next char as it is
 and return to start_state*/
 int state_left_slash(void)
 {
     
     int ch;
-    /*printf("state_left_slash() ");*/
     ch = getchar();
     if (ch == '\n')
     {
@@ -85,7 +81,6 @@ int state_left_slash(void)
             printf(" ");
             comment_start = error_line;
             state_reject();
-            /*printf("Error line is : %d", error_line);*/
             break;
         case '/':
             printf("/");
@@ -101,14 +96,9 @@ int state_left_slash(void)
             printf("\'");
             state_single_quote();
             break;
-        case '\n':
-            printf("/");
-            printf("\n");
-            start_state();
-            break;
         default:
             printf("/");
-            printf("%c",ch);
+            putchar(ch);
             start_state();
             break;  
     }
@@ -121,52 +111,15 @@ else
     return 0;
 
 }
-/*Gets the next character from input.
-Previous char is \.
-Print literal value of next char if it is a special char; 
-and return to start_state*/
-int state_escape_slash(void)
-{
-    int ch;
-    printf("\\");
-    /*printf("state_left_slash() ");*/
-    ch = getchar();
-    if (ch == '\n')
-    {
-        error_line=error_line+1;
-    }
-    if (ch != EOF){
-    switch(ch)
-    {
-        case '\'':
-            printf("\'");
-            start_state();
-            break;
-        case '\"':
-            printf("\"");
-            start_state();
-            break;
-        case '\\':
-            printf("\\");
-            start_state();
-            break;
-        default:
-            start_state();
-            break;  
-    }
-}
-    return 0;
-}
+
 
 /*Gets next char from input.
-Previous char is \ and in comment.
 Print literal value of next char if it is a special char; 
 and return to double quoted comment state*/ 
 int escape_dquoted(void)
 {
     int ch;
     printf("\\");
-    /*printf("escape_dquoted() ");*/
     ch = getchar();
     if (ch == '\n')
     {
@@ -197,13 +150,11 @@ int escape_dquoted(void)
 
 
 /*Gets next char from input.
-Inside double quoted comment
-Enter corresponding states if next char
-is \ or ", otherwise, print next char to output*/
+Termonates comment or enters corresponding states 
+if next char is \ or ", otherwise, print next char to output*/
 int state_double_quote(void)
 {
     int ch;
-    /*printf("double_quote() ");*/
     ch = getchar();
     if (ch == '\n')
     {
@@ -229,13 +180,11 @@ int state_double_quote(void)
 }
 
 /*Gets next char from input.
-Inside single quoted comment
-Enter corresponding states if next char
-is \ or ", otherwise, print next char to output*/
+Terminate comment or enter corresponding states 
+if next char is \ or ", otherwise, print next char to output*/
 int state_single_quote(void)
 {
     int ch;
-    /*printf("single_quote() ");*/
     ch = getchar();
     if (ch == '\n')
     {
@@ -262,7 +211,6 @@ return 0;
 }
 
 /*Gets next char from input.
-Previous char is \ and in comment.
 Print literal value of next char if it is a special char; 
 and return to single quoted comment*/  
 int escape_squoted(void)
@@ -270,7 +218,6 @@ int escape_squoted(void)
     int ch;
     printf("\\");
     
-    /*printf("escape_squoted() ");*/
     ch = getchar();
     if (ch == '\n')
     {
@@ -303,12 +250,11 @@ int escape_squoted(void)
 /*Get next char from input.
 Unless comment terminated, 
 print only new lines; 
-exit with an error if EOF before comment termination*/
+exit with an error if input ended before comment does*/
 int state_reject(void)
 {
     int ch;
 
-    /*printf("reject() ");*/
     ch = getchar();
     if (ch == '\n')
     {
@@ -322,7 +268,7 @@ int state_reject(void)
             state_aster();
             break;
         case '\n':
-            printf("\n");
+            putchar(ch);
             state_reject();
             break;
         default:
@@ -340,12 +286,11 @@ int state_reject(void)
 }
 /*Get next char in input.
 In comment state, unless exited by /.
-Exit with error if EOF before comment terminated*/
+Exit with error if input ended before comment does*/
 int state_aster(void)
 {
     int ch;
     
-    /*printf("aster() ");*/
     ch = getchar();
     if (ch == '\n')
     {
@@ -356,7 +301,6 @@ int state_aster(void)
     switch(ch)
     {
         case '/':
-            /*printf("%c",ch);*/
             start_state();
             break;
         case '*':
